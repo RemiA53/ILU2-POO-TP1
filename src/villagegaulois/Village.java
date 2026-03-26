d2fee99ae70ce2b0ee8a8588d8f1ccd068a57bc3
RemiA53 <remialibert690@gmail.com>
2026-03-26 17:55:16 +0100

    TODO

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -31,6 +31,9 @@ public class Village {
 
 		public Marche(int nbEtal) {
 			etals = new Etal[nbEtal];
+			for (int i = 0; i < nbEtal; i++) {
+				etals[i] = new Etal();
+			}
 		}
 
 		public void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
